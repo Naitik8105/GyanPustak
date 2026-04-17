7860aae3ffae8c7e19cc5d79108c52eb0f9753d5
Naitik <naitikkotadiya8105@gmail.com>
2026-04-17 16:43:07 +0530

    config changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
-import mysql from 'mysql2';
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-export default db;
+module.exports = pool;
